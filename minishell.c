97bcc2de20794b36d54284ec1263ca7f50293633
tkirmizi <t.kirmizioglu@outlook.com>
2024-08-30 13:17:59 +0200

    environment structure base and path finder added for one simple non-builtin execution

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: tkirmizi <tkirmizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 15:29:49 by tkirmizi          #+#    #+#             */
-/*   Updated: 2024/08/28 17:08:21 by tkirmizi         ###   ########.fr       */
+/*   Updated: 2024/08/30 13:13:16 by tkirmizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,55 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	
+	t_mini *mini;
+	while (1)
+	{
+		execution(mini->cmd);
+	}
 }
 
-int		command_counter(t_command *command)
+void	execution(t_mini *mini)
+{
+	int	count_command; // count
+	count_command = command_counter(mini->cmd);
+	if (count_command == 1) // one execution
+		one_exec(mini);
+	// else
+	//	execution with pipes.
+}
+
+int		command_counter(t_cmd *command)
 {
 	int	i;
 	
 	i = 0;
-	if (command -> args == NULL)
+	if (command -> args == NULL) // no args and what to do
 		return (i);
+	else if (command -> next == NULL && command -> args) 
+			return (1);
 	else
 	{
-		if (command -> next == NULL)
-			return (1);
-		else
+		while (command -> next != NULL)
 		{
-			while (command -> next != NULL)
-			{
-				i++;
-				command = command -> next;
-			}
-			return (i);
+			i++;
+			command = command -> next;
 		}
+		return (i + 1);
+	}
+}
+
+void	one_exec(t_mini *mini)
+{
+	char  *path = "PATH";
+	t_mini *temp = mini;
+	
+	while (temp->env_p != NULL)
+	{
+		if (ft_strcmp(temp->env_p->env_name, path) == 0)
+		{
+			printf("env name = %s\n", temp->env_p->env_name);
+			break;
+		}
+		temp->env_p = temp->env_p->next;
 	}
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: tkirmizi <tkirmizi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 18:30:22 by tkirmizi          #+#    #+#             */
-/*   Updated: 2024/08/28 17:08:51 by tkirmizi         ###   ########.fr       */
+/*   Updated: 2024/08/30 13:09:27 by tkirmizi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>  
 #include <signal.h>
-typedef struct s_command
+typedef struct s_cmd
 {
 	// t_token *token; lexer/parser partner will take care of it
 	struct s_mini *prev;
@@ -30,6 +30,20 @@ typedef struct s_command
 	int fd_in;
 	int fd_out;
 	char **args; 
-} t_command;
+}	t_cmd;
+
+typedef struct s_mini
+{
+	t_cmd	*cmd;
+	t_env *env_p;
+	// pid_t *pids;
+}	t_mini;
+
+typedef struct s_env
+{
+	char *env_name;
+	char *env_value;
+	struct s_env *next;
+}	t_env;
 
 #endif
